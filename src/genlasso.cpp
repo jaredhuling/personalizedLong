d8a89856ed547fe09bd8a44c146de97dc1cca22d
jaredhuling <huling@wisc.edu>
2018-03-28 22:11:23 -0400

    fix to dimension issue

--- src/genlasso.cpp
+++ src/genlasso.cpp
@@ -30,65 +30,65 @@ typedef Eigen::SparseMatrix<double> SpMat;
 inline void write_beta_matrix(SpMat &betas, int col, double beta0, SpVec &coef)
 {
     betas.insert(0, col) = beta0;
-    
+
     for(SpVec::InnerIterator iter(coef); iter; ++iter)
     {
         betas.insert(iter.index() + 1, col) = iter.value();
     }
 }
 
-RcppExport SEXP admm_genlasso(SEXP x_, 
-                              SEXP y_, 
+RcppExport SEXP admm_genlasso(SEXP x_,
+                              SEXP y_,
                               SEXP D_,
                               SEXP lambda_,
-                              SEXP nlambda_, 
+                              SEXP nlambda_,
                               SEXP lmin_ratio_,
-                              SEXP standardize_, 
+                              SEXP standardize_,
                               SEXP intercept_,
                               SEXP opts_)
 {
     BEGIN_RCPP
-    
+
     //Rcpp::NumericMatrix xx(x_);
     //Rcpp::NumericVector yy(y_);
-    
+
     Rcpp::NumericMatrix xx(x_);
     Rcpp::NumericVector yy(y_);
-    
+
     const int n = xx.rows();
     const int p = xx.cols();
-    
+
     MatrixXd datX(n, p);
     VectorXd datY(n);
-    
+
     // Copy data and convert type from double to float
     std::copy(xx.begin(), xx.end(), datX.data());
     std::copy(yy.begin(), yy.end(), datY.data());
-    
+
     //MatrixXd datX(as<MatrixXd>(x_));
     //VectorXd datY(as<VectorXd>(y_));
-    
+
     //const int n = datX.rows();
     //const int p = datX.cols();
-    
+
     const SpMat D(as<MSpMat>(D_));
-    
+
     const int M = D.rows();
-    
+
     //MatrixXf datX(n, p);
     //VectorXf datY(n);
-    
+
     // Copy data and convert type from double to float
     //std::copy(xx.begin(), xx.end(), datX.data());
     //std::copy(yy.begin(), yy.end(), datY.data());
-    
+
     // In glmnet, we minimize
     //   1/(2n) * ||y - X * beta||^2 + lambda * ||beta||_1
     // which is equivalent to minimizing
     //   1/2 * ||y - X * beta||^2 + n * lambda * ||beta||_1
     ArrayXd lambda(as<ArrayXd>(lambda_));
     int nlambda = lambda.size();
-    
+
     List opts(opts_);
     const int maxit        = as<int>(opts["maxit"]);
     const double eps_abs   = as<double>(opts["eps_abs"]);
@@ -96,14 +96,14 @@ RcppExport SEXP admm_genlasso(SEXP x_,
     const double rho       = as<double>(opts["rho"]);
     const bool standardize = as<bool>(standardize_);
     const bool intercept   = as<bool>(intercept_);
-    
+
     DataStd<double> datstd(n, p, standardize, intercept);
     datstd.standardize(datX, datY);
-    
+
     ADMMGenLassoTall *solver_tall;
     //ADMMGenLassoWide *solver_wide;
-    
-    if(n > p)
+
+    if(n * 2 + 15 > p)
     {
         solver_tall = new ADMMGenLassoTall(datX, datY, D, eps_abs, eps_rel);
     }
@@ -111,11 +111,11 @@ RcppExport SEXP admm_genlasso(SEXP x_,
     {
         //solver_wide = new ADMMGenLassoWide(datX, datY, eps_abs, eps_rel);
     }
-    
+
     if(nlambda < 1)
     {
         double lmax = 0.0;
-        if(n > p)
+        if(n * 2 + 15 > p)
         {
             lmax = solver_tall->get_lambda_zero() / n * datstd.get_scaleY();
         }
@@ -129,25 +129,25 @@ RcppExport SEXP admm_genlasso(SEXP x_,
         lambda = lambda.exp();
         nlambda = lambda.size();
     }
-    
+
     MatrixXd beta(p + 1, nlambda);
     SpMat beta_aug(M + 1, nlambda);
     //beta.reserve(Eigen::VectorXi::Constant(nlambda, std::min(n, p)));
     beta_aug.reserve(Eigen::VectorXi::Constant(nlambda, std::min(n, M)));
-    
+
     IntegerVector niter(nlambda);
     double ilambda = 0.0;
-    
+
     for(int i = 0; i < nlambda; i++)
     {
         ilambda = lambda[i] * n / datstd.get_scaleY();
-        if(n > p)
+        if(n * 2 + 15 > p)
         {
             if(i == 0)
                 solver_tall->init(ilambda, rho);
             else
                 solver_tall->init_warm(ilambda);
-            
+
             niter[i] = solver_tall->solve(maxit);
             SpVec res = solver_tall->get_gamma();
             VectorXd restrue = solver_tall->get_beta();
@@ -165,7 +165,7 @@ RcppExport SEXP admm_genlasso(SEXP x_,
                 solver_wide->init(ilambda, rho);
             else
                 solver_wide->init_warm(ilambda);
-            
+
             niter[i] = solver_wide->solve(maxit);
             SpVec res = solver_wide->get_x();
             double beta0 = 0.0;
@@ -174,8 +174,8 @@ RcppExport SEXP admm_genlasso(SEXP x_,
              */
         }
     }
-    
-    if(n > p)
+
+    if(n * 2 + 15 > p)
     {
         delete solver_tall;
     }
@@ -183,14 +183,14 @@ RcppExport SEXP admm_genlasso(SEXP x_,
     {
         //delete solver_wide;
     }
-    
+
     beta_aug.makeCompressed();
-    
+
     return List::create(Named("lambda") = lambda,
                         Named("beta") = beta,
                         Named("beta.aug") = beta_aug,
                         Named("niter") = niter);
-    
+
     END_RCPP
 }
 
